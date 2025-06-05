75bde0272750a6396975b0836cc2aadaf35b81f1
adith2005-20 <132918418+adith2005-20@users.noreply.github.com>
2025-06-05 20:39:08 +0530

    fix: url for better auth was missing

--- impromptu/src/lib/auth-client.ts
+++ impromptu/src/lib/auth-client.ts
@@ -1,4 +1,6 @@
 import { createAuthClient } from "better-auth/react"
-export const authClient = createAuthClient()
+export const authClient = createAuthClient({
+    baseURL:"https://impromptu.adith.me"
+})
 
-export const { signIn, signUp, useSession } = createAuthClient()
+export const { signIn, signUp, useSession } = authClient
